721fb3e24d3f20568d8c67e831ddc69bbb0140ee
Erick Wilson <erick.97-11@hotmail.com>
2022-04-06 21:26:10 -0300

    commit inicial

---
+++ Js/main.js
@@ -0,0 +1,28 @@
+async function getContent() {
+    try {
+        const response = await fetch(' https://api.nomics.com/v1/currencies/ticker?key=60a3f926a3815abee088f22dd3f3cb564c8d4ab8&per-page=10&interval=1h')
+            //console.log(response)
+        const data = await response.json()
+        show(data)
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+getContent()
+
+function show(cryptos) {
+
+    let output = ''
+    let contador = 1;
+    for (let crypto of cryptos) {
+
+        output += `<div id="contador">${contador} - ${crypto.id} - ${crypto.name}</li>`
+        output += `<br>`
+        output += `<label>Price: $ ${crypto.price}</label>`
+        output += `</div>`
+        output += `<br>`
+        contador++;
+    }
+    document.querySelector('#divTop10').innerHTML = output
+}
